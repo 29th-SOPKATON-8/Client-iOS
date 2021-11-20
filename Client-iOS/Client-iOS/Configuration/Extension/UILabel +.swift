8f841f7271798fc5b28ad1e5468327137d11872c
heerucan <iwpow0307@gmail.com>
2021-11-21 06:45:05 +0900

    [#8] CompleteVC UI 구현

--- Client-iOS/Client-iOS/Configuration/Extension/UILabel +.swift
+++ Client-iOS/Client-iOS/Configuration/Extension/UILabel +.swift
@@ -30,4 +30,13 @@ extension UILabel {
         
         return label.bounds.size
     }
+    
+    func addCharacterColor(color: UIColor, range: String) {
+        if let labelText = text, labelText.count > 0 {
+            let attributedStr = NSMutableAttributedString(string: labelText)
+            attributedStr.addAttribute(.foregroundColor, value: color, range: (labelText as NSString).range(of: range))
+            attributedStr.addAttribute(.font, value: UIFont.systemFont(ofSize: 24, weight: .semibold), range: (labelText as NSString).range(of: range))
+            attributedText = attributedStr
+        }
+    }
 }
