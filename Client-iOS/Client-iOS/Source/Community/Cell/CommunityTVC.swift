2692f65fd18bb176a8f10a3ff475f664a9dde7a0
hyesuuou <68391767+hyesuuou@users.noreply.github.com>
2021-11-21 04:00:41 +0900

    [#4] 커뮤니티뷰 전체적인 UI짜기

--- Client-iOS/Client-iOS/Source/Community/Cell/CommunityTVC.swift
+++ Client-iOS/Client-iOS/Source/Community/Cell/CommunityTVC.swift
@@ -8,16 +8,24 @@
 import UIKit
 
 class CommunityTVC: UITableViewCell {
+    
+    public static let identifier = "CommunityTVC"
 
+    @IBOutlet weak var bgView: UIView!
     override func awakeFromNib() {
         super.awakeFromNib()
-        // Initialization code
+        setUI()
     }
 
     override func setSelected(_ selected: Bool, animated: Bool) {
         super.setSelected(selected, animated: animated)
 
         // Configure the view for the selected state
+    }
+    
+    func setUI() {
+        bgView.layer.cornerRadius = 20
+        bgView.setShadow(radius: 2, offset: CGSize(width: 0, height: 2), opacity: 0.25)
     }
 
 }
