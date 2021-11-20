5c3e555b17df9f168541145a536467cd658b687d
hyesuuou <68391767+hyesuuou@users.noreply.github.com>
2021-11-21 02:40:13 +0900

    [#1] extension 추가

--- Client-iOS/Client-iOS/Configuration/Extension/UIViewController +.swift
+++ Client-iOS/Client-iOS/Configuration/Extension/UIViewController +.swift
@@ -8,6 +8,21 @@
 import UIKit
 
 extension UIViewController {
+    
+    // MARK: - 토스트
+    func showToast(message : String, font: UIFont) {
+            let toastLabel = UILabel(frame: CGRect(x: self.view.frame.size.width/2 - 75, y: self.view.frame.size.height-100, width: 150, height: 35))
+            toastLabel.backgroundColor = UIColor.black.withAlphaComponent(0.6)
+            toastLabel.textColor = UIColor.white
+            toastLabel.font = font
+            toastLabel.textAlignment = .center;
+            toastLabel.text = message
+            toastLabel.alpha = 1.0
+            toastLabel.layer.cornerRadius = 10;
+            toastLabel.clipsToBounds = true
+            self.view.addSubview(toastLabel)
+            UIView.animate(withDuration: 4.0, delay: 0.1, options: .curveEaseOut, animations: { toastLabel.alpha = 0.0 }, completion: {(isCompleted) in toastLabel.removeFromSuperview() }) }
+    
     func hideKeyboard() {
         let tap: UITapGestureRecognizer = UITapGestureRecognizer (target: self,
                                                                   action: #selector(UIViewController.dismissKeyboard))
